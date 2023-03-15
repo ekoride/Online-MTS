0f417715e116e34d265548bcdfacbd9a245a8ebe
ekoride <eshwar.koride7@gmail.com>
2023-03-14 23:07:29 -0700

    Movie entity is modified

--- MTS-Backend/src/main/java/com/prayesh/mts/entity/Movie.java
+++ MTS-Backend/src/main/java/com/prayesh/mts/entity/Movie.java
@@ -1,6 +1,7 @@
 package com.prayesh.mts.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +11,15 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -46,18 +51,24 @@ public class Movie {
     @Column(name = "movie_rating")
     private double movieRating;
     @Column(name = "movie_genre")
+    @NotEmpty(message = "Genre cannot be empty")
     private String movieGenre;
     @Column(name = "movie_language")
-    private String movieLangauge;
+    @NotEmpty(message = "Language cannot be empty")
+    private String movieLanguage;
     @Column(name = "movie_description")
     private String movieDescription;
-    @Column(name = "movie_is_available", nullable = false)
-    private boolean isMovieAvailable;
+    @Column(name = "movie_releaseDate")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "Release Date cannot be null")
+    private Date movieReleaseDate;
     @Column(name = "movie_duration")
-    @NotNull(message = "Enter movie duration")
+    @NotEmpty(message = "Enter movie duration")
     private int movieDurationMin;
-    @Column(name = "movie_available_till", nullable = false)
-    private int movieTicketsAvailableTill;
+    @Column(name = "movie_endDate")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "End Date cannot be null")
+    private Date movieEndDate;
 
     // private Date startDate;   AddMovie API -> Sets the current date to start date
     // private Date endDate;     DeleteMovie API -> Sets the current date to end date 
