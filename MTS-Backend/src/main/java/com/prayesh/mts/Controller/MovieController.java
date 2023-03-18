91891f07f15fe80c14052301afe62e277cfa90d3
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2023-03-17 22:18:03 -0700

    Added get and post requests for movies on frontend

--- MTS-Backend/src/main/java/com/prayesh/mts/Controller/MovieController.java
+++ MTS-Backend/src/main/java/com/prayesh/mts/Controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -69,12 +70,13 @@ public class MovieController {
         return ResponseEntity.ok(movieService.getUpcomingMovies());
     }
 
-
+    @CrossOrigin(origins="http://localhost:3000/")
     @GetMapping("/getAllMovies")
     public ResponseEntity<List<Movie>> getAllMovies(){
         return ResponseEntity.ok(movieService.getCurrentMovies()); // What if no movies are returned, Error?
     }
     
+    @CrossOrigin(origins="http://localhost:3000/")
     @PostMapping("/addNewMovie")
     public ResponseEntity<Movie> postMovie(@RequestBody @Valid Movie movie){
         return new ResponseEntity<>(movieService.saveMovie(movie), HttpStatus.CREATED);
--- MTS-Frontend/src/components/Welcome.js
+++ MTS-Frontend/src/components/Welcome.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Welcome() {
   return (
     <div class="jumbotron m-5">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <h1 class="display-4">Hello, Folks!</h1>
+            <p class="lead">This is a site where you can book tickets and read reviews about movies</p>
             <hr class="my-4"></hr>
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p>Movies of all genres and languages available.</p>
     </div>
   )
 }
